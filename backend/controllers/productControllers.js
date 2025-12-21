752a9663db010eadbd21507b8f6d491b61c7c433
SamarSaeed1146 <samarsaeed374@gmail.com>
2025-12-21 14:48:28 +0500

    Update

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,3 +1,15 @@
+import Product from "../models/product.js";
+
+// create new product => /api/v1/products
+
 export const getProducts = async (req, res) => {
   res.status(200).json({ message: "All products" });
+};
+
+// Create new product => /api/v1/admin/products
+export const newProducts = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(200).json({
+    product,
+  });
 };
