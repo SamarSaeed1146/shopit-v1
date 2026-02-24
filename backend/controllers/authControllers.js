9183a04264ebaa7d68a4aadba81dd26f323a91b7
SamarSaeed1146 <samarsaeed374@gmail.com>
2026-02-24 16:51:25 +0500

    Update the Links

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -81,7 +81,7 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
   await user.save();
 
-  const resetUrl = `${process.env.FRONTEND_URL}/api/v1/password/reset/${resetToken}`;
+  const resetUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
   const message = getResetPasswordTemplate(user?.name, resetUrl);
   try {
     await sendEmail({
