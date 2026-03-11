b93a32cd9a7c04f69ff0e48adb1f063ccebcc0fa
SamarSaeed1146 <samarsaeed374@gmail.com>
2026-03-11 16:16:26 +0500

    Update The MetaData

--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -3,6 +3,7 @@ import { useRegisterMutation } from "../../redux/api/authApi";
 import { toast } from "react-hot-toast";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import MetaData from "../layout/MetaData";
 
 function Register() {
   const [user, setUser] = useState({
@@ -46,64 +47,67 @@ function Register() {
   };
 
   return (
-    <div className="row wrapper">
-      <div className="col-10 col-lg-5">
-        <form className="shadow rounded bg-body" onSubmit={submitHandler}>
-          <h2 className="mb-4">Register</h2>
+    <>
+      <MetaData title={"Register"} />
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form className="shadow rounded bg-body" onSubmit={submitHandler}>
+            <h2 className="mb-4">Register</h2>
 
-          <div className="mb-3">
-            <label htmlFor="name_field" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name_field"
-              className="form-control"
-              name="name"
-              value={name}
-              onChange={onChange}
-            />
-          </div>
+            <div className="mb-3">
+              <label htmlFor="name_field" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name_field"
+                className="form-control"
+                name="name"
+                value={name}
+                onChange={onChange}
+              />
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="email_field" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email_field"
-              className="form-control"
-              name="email"
-              value={email}
-              onChange={onChange}
-            />
-          </div>
+            <div className="mb-3">
+              <label htmlFor="email_field" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email_field"
+                className="form-control"
+                name="email"
+                value={email}
+                onChange={onChange}
+              />
+            </div>
 
-          <div className="mb-3">
-            <label for="password_field" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password_field"
-              className="form-control"
-              name="password"
-              value={password}
-              onChange={onChange}
-            />
-          </div>
+            <div className="mb-3">
+              <label for="password_field" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password_field"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={onChange}
+              />
+            </div>
 
-          <button
-            id="register_button"
-            type="submit"
-            className="btn w-100 py-2"
-            disabled={isLoading}
-          >
-            {isLoading ? "Registering..." : "REGISTER"} REGISTER
-          </button>
-        </form>
+            <button
+              id="register_button"
+              type="submit"
+              className="btn w-100 py-2"
+              disabled={isLoading}
+            >
+              {isLoading ? "Registering..." : "REGISTER"} REGISTER
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
