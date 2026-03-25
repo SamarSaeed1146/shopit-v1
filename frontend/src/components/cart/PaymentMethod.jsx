b4f9727758b952bd17766866c8e61ebab82fc525
SamarSaeed1146 <samarsaeed374@gmail.com>
2026-03-25 23:50:08 +0500

    Added Stripe Function In Frontend

--- frontend/src/components/cart/PaymentMethod.jsx
+++ frontend/src/components/cart/PaymentMethod.jsx
@@ -3,7 +3,10 @@ import { useSelector } from "react-redux";
 import CheckoutSteps from "./CheckoutSteps";
 import { useEffect, useState } from "react";
 import { calculateOrderCost } from "../../helpers/helpers";
-import { useCreateNewOrderMutation } from "../../redux/api/orderApi";
+import {
+  useCreateNewOrderMutation,
+  useStripeCheckoutSessionMutation,
+} from "../../redux/api/orderApi";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
@@ -15,6 +18,21 @@ function PaymentMethod() {
   const { shippingInfo, cartItems } = useSelector((state) => state.cart);
 
   const [createNewOrder, { error, isSuccess }] = useCreateNewOrderMutation();
+
+  const [
+    stripeCheckoutSession,
+    { data: checkoutData, error: checkoutError, isLoading },
+  ] = useStripeCheckoutSessionMutation();
+
+  useEffect(() => {
+    if (checkoutData) {
+      window.location.href = checkoutData?.url;
+    }
+
+    if (checkoutError) {
+      toast.error(checkoutError?.data?.message);
+    }
+  }, [checkoutData, checkoutError, isLoading, navigate]);
 
   useEffect(() => {
     if (error) {
@@ -49,7 +67,16 @@ function PaymentMethod() {
     }
 
     if (method === "Card") {
-      alert("Card");
+      const orderData = {
+        shippingInfo,
+        orderItems: cartItems,
+        itemsPrice,
+        shippingAmount: shippingPrice,
+        taxAmount: taxPrice,
+        totalAmount: totalPrice,
+      };
+
+      stripeCheckoutSession(orderData);
     }
   };
 
@@ -89,7 +116,12 @@ function PaymentMethod() {
               </label>
             </div>
 
-            <button id="shipping_btn" type="submit" className="btn py-2 w-100">
+            <button
+              id="shipping_btn"
+              type="submit"
+              className="btn py-2 w-100"
+              disabled={isLoading}
+            >
               CONTINUE
             </button>
           </form>
